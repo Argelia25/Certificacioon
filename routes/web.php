59ad5fd8a292c607792517c33096fffac8ac1dca
Luz pérez <aregelianajera23@gmail.com>
2020-10-10 17:13:24 -0600

    Realizacion de rutas de cada tabla

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes(['register'=>false,'reset'=>false]);
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', 'PrestamoController@index')->name('home');
+
+Route::resource('libros', 'LibrosController')->middleware('auth');
+//Recursos para se generen las rutas necesarias de autores y solo sean accesibles si ya se paso el middleware
+Route::resource('autores', 'AutoresController')->middleware('auth');
+
+//Recursos para se generen las rutas necesarias de categorias y solo sean accesibles si ya se paso el middleware
+Route::resource('ctegorias', 'CtegoriasController')->middleware('auth');
+
+//Recursos para se generen las rutas necesarias de editoriales y solo sean accesibles si ya se paso el middleware
+Route::resource('editorial', 'EditorialController')->middleware('auth');
+
+//Recursos para se generen las rutas necesarias de prestamos y solo sean accesibles si ya se paso el middleware
+Route::resource('prestamo', 'PrestamoController')->middleware('auth');
+//Recursos para se generen las rutas necesarias de usuarios y solo sean accesibles si ya se paso el middleware
+Route::resource('usuario', 'UsuarioController')->middleware('auth');
+//Recursos para se generen las rutas necesarias de libros y solo sean accesibles si ya se paso el middleware
+Route::resource('libros', 'LibrosController')->middleware('auth');
+
+Route::get('/prueba', 'reporteController@PDFprestamo')->name('descargarpdf');
