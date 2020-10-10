f4462d0f97b6b595d0f9e5fad3ae3d1edaf4393d
Luz pérez <aregelianajera23@gmail.com>
2020-10-10 17:17:15 -0600

    Modificacion del controlador dectegorias

--- app/Http/Controllers/CtegoriasController.php
+++ app/Http/Controllers/CtegoriasController.php
@@ -10,11 +10,7 @@ use Illuminate\Http\Request;
 
 class CtegoriasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\View\View
-     */
+   
     public function index(Request $request)
     {
         $keyword = $request->get('search');
@@ -30,23 +26,13 @@ class CtegoriasController extends Controller
         return view('ctegorias.index', compact('ctegorias'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\View\View
-     */
+  
     public function create()
     {
         return view('ctegorias.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
+  
     public function store(Request $request)
     {
         
@@ -57,13 +43,7 @@ class CtegoriasController extends Controller
         return redirect('ctegorias')->with('flash_message', 'Ctegoria added!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\View\View
-     */
+   
     public function show($id)
     {
         $ctegoria = Ctegoria::findOrFail($id);
@@ -71,13 +51,7 @@ class CtegoriasController extends Controller
         return view('ctegorias.show', compact('ctegoria'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\View\View
-     */
+  
     public function edit($id)
     {
         $ctegoria = Ctegoria::findOrFail($id);
@@ -85,14 +59,7 @@ class CtegoriasController extends Controller
         return view('ctegorias.edit', compact('ctegoria'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
+    
     public function update(Request $request, $id)
     {
         
@@ -104,13 +71,7 @@ class CtegoriasController extends Controller
         return redirect('ctegorias')->with('flash_message', 'Ctegoria updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
+ 
     public function destroy($id)
     {
         Ctegoria::destroy($id);
