54722617a4f47b093cfe6c1f38bb2b9129cc2d70
Luz pérez <aregelianajera23@gmail.com>
2020-10-10 17:18:26 -0600

    modificacion de controlador de editoriales

--- app/Http/Controllers/EditorialController.php
+++ app/Http/Controllers/EditorialController.php
@@ -10,11 +10,7 @@ use Illuminate\Http\Request;
 
 class EditorialController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\View\View
-     */
+   
     public function index(Request $request)
     {
         $keyword = $request->get('search');
@@ -30,23 +26,12 @@ class EditorialController extends Controller
         return view('editorial.index', compact('editorial'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\View\View
-     */
+    
     public function create()
     {
         return view('editorial.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
     public function store(Request $request)
     {
         
@@ -57,13 +42,6 @@ class EditorialController extends Controller
         return redirect('editorial')->with('flash_message', 'Editorial added!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\View\View
-     */
     public function show($id)
     {
         $editorial = Editorial::findOrFail($id);
@@ -71,13 +49,7 @@ class EditorialController extends Controller
         return view('editorial.show', compact('editorial'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\View\View
-     */
+   
     public function edit($id)
     {
         $editorial = Editorial::findOrFail($id);
@@ -85,14 +57,7 @@ class EditorialController extends Controller
         return view('editorial.edit', compact('editorial'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
+    
     public function update(Request $request, $id)
     {
         
@@ -104,13 +69,7 @@ class EditorialController extends Controller
         return redirect('editorial')->with('flash_message', 'Editorial updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
+   
     public function destroy($id)
     {
         Editorial::destroy($id);
