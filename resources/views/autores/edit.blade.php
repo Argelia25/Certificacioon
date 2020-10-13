d7f4fdfcfb82e923468265288beacb3f2aded33b
Luz pérez <aregelianajera23@gmail.com>
2020-10-13 16:48:55 -0600

    se modifico la vista de editar autores colocando color.

--- resources/views/autores/edit.blade.php
+++ resources/views/autores/edit.blade.php
@@ -12,7 +12,7 @@
                   
 
                         @if ($errors->any())
-                            <ul class="alert alert-danger">
+               cCcqw             <ul class="alert alert-danger">
                                 @foreach ($errors->all() as $error)
                                     <li>{{ $error }}</li>
                                 @endforeach
